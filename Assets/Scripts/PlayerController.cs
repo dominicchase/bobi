43801cc848d125133b341403cabe9068a79b976a
Dominic Peterson <dominicpeterson@Dominics-iMac.local>
2022-07-03 21:09:04 -0400

    Player controller and camera box

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private Camera mainCamera;
+
+    void Start() { }
 
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            player.SetActive(true);
+            movePlayer();
+        }
+        else if (!Input.GetMouseButtonUp(0))
+            player.SetActive(false);
+    }
+
+    void movePlayer()
+    {
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        worldPosition.z = 0f;
+        transform.position = worldPosition;
     }
 }
