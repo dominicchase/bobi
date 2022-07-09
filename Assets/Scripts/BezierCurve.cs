1302064db8ea1839dbb150bc1564e2ed97e6cf7e
Dominic Peterson <dominicpeterson@Dominics-iMac.local>
2022-07-09 00:04:41 -0400

    BezierCurve

---
+++ Assets/Scripts/BezierCurve.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BezierCurve : MonoBehaviour {
+
+    public LineRenderer lineRenderer;
+    [SerializeField] bool showLineRenderer = true;
+
+    [SerializeField] AnimationCurve curve;
+
+    private Vector3[] smoothPoints;
+    private int smoothIdx = 1;
+
+    Rigidbody2D rb;
+    public GameObject tmp;
+
+    void Start() {
+        rb = tmp.GetComponent<Rigidbody2D>();
+        // get first curve
+        smoothPoints = getCurve(getBezierPoints(), 0.1f).ToArray();
+        StartCoroutine(wander());
+    }
+
+    void Update() { }
+
+    void FixedUpdate() { }
+
+    IEnumerator wander() {
+        while (smoothIdx < smoothPoints.Length) {
+            if (smoothIdx.Equals(smoothPoints.Length - 1)) {
+                smoothPoints = getCurve(getBezierPoints(), 0.5f).ToArray();
+                smoothIdx = 1;
+            }
+
+            Debug.Log(smoothIdx);
+
+            Vector2 nextPoint = Vector2.MoveTowards(
+                smoothPoints[smoothIdx - 1], 
+                smoothPoints[smoothIdx], 
+                Time.fixedDeltaTime * 0.25f
+            );
+
+            rb.MovePosition(nextPoint);
+                
+            smoothIdx += 1;
+
+            yield return new WaitForSecondsRealtime(curve.Evaluate(smoothIdx / smoothPoints.Length) * 1000);
+        }
+    }
+
+    private Vector2 getQuadraticBezier(Vector3 a, Vector3 b, Vector3 c, float t) {
+        Vector2 p0 = Vector2.Lerp(a, b, t);
+        Vector2 p1 = Vector2.Lerp(b, c, t);
+
+        return Vector2.Lerp(p0, p1, t);
+    }
+
+    private Vector2 getCubicBezier(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t) {
+        Vector2 p0 = getQuadraticBezier(a, b, c, t);
+        Vector2 p1 = getQuadraticBezier(b, c, d, t);
+
+        return Vector2.Lerp(p0, p1, t);
+    }
+
+    private Vector3[] getAnchorPoints() {
+        Vector3[] anchorPoints = new Vector3[3];
+
+        for (int i = 0; i < 3; i++) {
+            anchorPoints[i] = new Vector3(Random.Range(-2, 2), Random.Range(-4, 4), 0);
+
+            foreach (Vector3 item in anchorPoints) 
+                if (anchorPoints[i].Equals(item))
+                    anchorPoints[i] = new Vector3(Random.Range(-2, 2), Random.Range(-4, 4), 0);
+        }
+
+        return anchorPoints;
+    }
+
+    private Vector3[] getBezierPoints() {
+        Vector3[] bezierPoints = new Vector3[50];
+
+        for (int i = 1; i < bezierPoints.Length + 1; i++) {
+            float t = i / (float)bezierPoints.Length;
+            bezierPoints[i - 1] = getCubicBezier(
+                transform.position,getAnchorPoints()[0], 
+                getAnchorPoints()[1], 
+                getAnchorPoints()[2], t
+            );
+        }
+
+        return bezierPoints;
+    }
+
+    public List<Vector3> getCurve(Vector3[] bezierPoints, float spacing) {
+        List<Vector3> result = new List<Vector3>();
+
+        spacing = spacing > 0.00001f ? spacing : 0.00001f;
+
+        float offset = 0;
+        while (offset < 0)
+            offset += spacing;
+
+        Vector2 current = bezierPoints[0];
+        Vector2 next = bezierPoints[1];
+        int last = bezierPoints.Length - 1;
+
+        int i = 1;
+        while (true) {
+            Vector2 diff = next - current;
+            float dist = diff.magnitude;
+
+            if (dist >= offset) {
+                current += diff * (offset / dist);
+                result.Add(new Vector3(current.x, current.y, 0f));
+                offset = spacing;
+            }
+
+            else if (i != last) {
+                offset -= dist;
+                current = next;
+                next = bezierPoints[i++];
+            }
+
+            else break;
+        }
+
+        lineRenderer.positionCount = 50;
+        lineRenderer.SetPositions(result.ToArray());
+
+        return result;
+    }
+}
--- Assets/Scripts/FriendlyController.cs
+++ Assets/Scripts/FriendlyController.cs
@@ -6,109 +6,34 @@ using UnityEngine.AI;
 public class FriendlyController : MonoBehaviour
 {
     Rigidbody2D rb;
-
     public GameObject player;
-
+    Vector2 nextPoint = new Vector2(0f, 0f);
     [SerializeField] float speed;
-    private bool hasArrived = true;
-
-    [SerializeField] bool showLineRenderer;
-    public LineRenderer lineRenderer;
-    private int numPoints = 1000;
-    private Vector3[] points = new Vector3[1000];
-
-    // TODO: 
-    // - if player holds down, reset bevier curve from friend's new position
-    // - make sure friend can collider with objs (dynamic/kinematic)
-    // - make sure if friend collides with obj, recalc bezier curve
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update() { }
+    void Update() { 
+        getNextPoint();
+    }
 
     void FixedUpdate() {
         if (Input.GetMouseButton(0)) {
             if (Vector2.Distance(transform.position, player.transform.position) > 0.25f) { 
                 follow();
-                Debug.Log(Vector2.Distance(transform.position, player.transform.position) > 0.5f);
-                Debug.Log(Vector2.Distance(transform.position, player.transform.position));
             }
-        }
-
-        else if (!Input.GetMouseButton(0) && hasArrived) {
-            setBezierCurve();
-            StartCoroutine(wander());
         }
     }
 
     #region friend actions
 
+    void getNextPoint() {
+        Vector2 nextPoint = Vector2.Lerp(transform.position, player.transform.position, Time.deltaTime);
+    }
+
     void follow() {
-        Vector2 nextPointFollow = Vector2.Lerp(transform.position, player.transform.position, 1 * Time.fixedDeltaTime);
-        rb.MovePosition(nextPointFollow);
-    }
-
-    IEnumerator wander() {
-        hasArrived = false;
-
-        for (int i = 1; i < points.Length; i++) {
-            if (Input.GetMouseButton(0)) break;
-            Vector2 nextPointWander = Vector2.MoveTowards(
-                points[i - 1], 
-                points[i], 
-                Vector2.Distance(points[i - 1], points[i])
-            );
-            rb.MovePosition(nextPointWander);
-            yield return new WaitForSeconds(Time.fixedDeltaTime);
-        }
-
-        hasArrived = true;
-    }
-
-    #endregion
-
-    #region bezier curve functions
-
-    // quadratic
-    // private Vector3 getBezierPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t) {
-    //     return (((1 - t) * (1 - t)) * p0) + (2 * (1 - t) * t * p1) + (t * t * p2);
-    // }
-
-    // cubic
-    private Vector3 getBezierPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
-        return (((1 - t) * (1 - t) * (1 - t)) * p0) + (3 * ((1 - t) * (1 - t)) * t * p1) + (3 * (1 - t) * t * t * p2) + (t * t * t * p3);
-    }
-
-    private void setBezierCurve() {
-        GameObject p1 = new GameObject();
-        p1.transform.position = new Vector3(Random.Range(-2, 2), Random.Range(-4, 4), 0);
-
-        GameObject p2 = new GameObject();
-        p2.transform.position = new Vector3(Random.Range(-2, 2), Random.Range(-4, 4), 0);
-
-        GameObject p3 = new GameObject();
-        p3.transform.position = new Vector3(Random.Range(-2, 2), Random.Range(-4, 4), 0);
-
-        if (showLineRenderer) lineRenderer.positionCount = numPoints;
-
-        for (int i = 1; i < numPoints + 1; i++) {
-            float t = i / (float)numPoints;
-            points[i - 1] = getBezierPoint(
-                transform.position, 
-                p1.transform.position, 
-                p2.transform.position, 
-                p3.transform.position, 
-                t
-            );
-        }
-
-        if (showLineRenderer) lineRenderer.SetPositions(points);
-
-        Destroy(p1);
-        Destroy(p2);
-        Destroy(p3);
+        rb.MovePosition(nextPoint);
     }
 
     #endregion
